6836a120497a83c2e69cacb580244531725ad876
salg <devjason@gmail.com>
2023-01-04 21:26:51 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,5 +13,12 @@ function login() {
     id: id.value,
     psword: password.value
   };
-  console.log(req);
+  
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  });
 }
